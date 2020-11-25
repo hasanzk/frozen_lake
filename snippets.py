0916fb4626668740dbae4e7c91e7d6dda67ab0cb
Abdullah Alhassan <atalhassan@gmail.com>
2020-11-25 21:02:55 +0300

    added starting point call and dummy code to bypass syntax error

--- snippets.py
+++ snippets.py
@@ -4,153 +4,158 @@ import numpy as np
 import contextlib
 
 # Configures numpy print options
+
+
 @contextlib.contextmanager
 def _printoptions(*args, **kwargs):
     original = np.get_printoptions()
     np.set_printoptions(*args, **kwargs)
     try:
         yield
-    finally: 
+    finally:
         np.set_printoptions(**original)
 
-        
+
 class EnvironmentModel:
     def __init__(self, n_states, n_actions, seed=None):
         self.n_states = n_states
         self.n_actions = n_actions
-        
+
         self.random_state = np.random.RandomState(seed)
-        
+
     def p(self, next_state, state, action):
         raise NotImplementedError()
-    
+
     def r(self, next_state, state, action):
         raise NotImplementedError()
-        
+
     def draw(self, state, action):
         p = [self.p(ns, state, action) for ns in range(self.n_states)]
         next_state = self.random_state.choice(self.n_states, p=p)
         reward = self.r(next_state, state, action)
-        
+
         return next_state, reward
 
-        
+
 class Environment(EnvironmentModel):
     def __init__(self, n_states, n_actions, max_steps, pi, seed=None):
         EnvironmentModel.__init__(self, n_states, n_actions, seed)
-        
+
         self.max_steps = max_steps
-        
+
         self.pi = pi
         if self.pi is None:
             self.pi = np.full(n_states, 1./n_states)
-        
+
     def reset(self):
         self.n_steps = 0
         self.state = self.random_state.choice(self.n_states, p=self.pi)
-        
+
         return self.state
-        
+
     def step(self, action):
         if action < 0 or action >= self.n_actions:
             raise Exception('Invalid action.')
-        
+
         self.n_steps += 1
         done = (self.n_steps >= self.max_steps)
-        
+
         self.state, reward = self.draw(self.state, action)
-        
+
         return self.state, reward, done
-    
+
     def render(self, policy=None, value=None):
         raise NotImplementedError()
 
-        
+
 class FrozenLake(Environment):
     def __init__(self, lake, slip, max_steps, seed=None):
-    """
-    lake: A matrix that represents the lake. For example:
-     lake =  [['&', '.', '.', '.'],
-              ['.', '#', '.', '#'],
-              ['.', '.', '.', '#'],
-              ['#', '.', '.', '$']]
-    slip: The probability that the agent will slip
-    max_steps: The maximum number of time steps in an episode
-    seed: A seed to control the random number generator (optional)
-    """
+        """
+        lake: A matrix that represents the lake. For example:
+        lake =  [['&', '.', '.', '.'],
+                ['.', '#', '.', '#'],
+                ['.', '.', '.', '#'],
+                ['#', '.', '.', '$']]
+        slip: The probability that the agent will slip
+        max_steps: The maximum number of time steps in an episode
+        seed: A seed to control the random number generator (optional)
+        """
         # start (&), frozen (.), hole (#), goal ($)
         self.lake = np.array(lake)
         self.lake_flat = self.lake.reshape(-1)
-        
+
         self.slip = slip
-        
+
         n_states = self.lake.size + 1
         n_actions = 4
-        
+
         pi = np.zeros(n_states, dtype=float)
         pi[np.where(self.lake_flat == '&')[0]] = 1.0
-        
+
         self.absorbing_state = n_states - 1
-        
+
         # TODO:
-        
+
     def step(self, action):
         state, reward, done = Environment.step(self, action)
-        
+
         done = (state == self.absorbing_state) or done
-        
+
         return state, reward, done
-        
+
     def p(self, next_state, state, action):
         # TODO:
-    
+        print("p")# dummy code add by Abdullah
+
     def r(self, next_state, state, action):
         # TODO:
-   
+        print("r")# dummy code add by Abdullah
+
     def render(self, policy=None, value=None):
         if policy is None:
             lake = np.array(self.lake_flat)
-            
+
             if self.state < self.absorbing_state:
                 lake[self.state] = '@'
-                
+
             print(lake.reshape(self.lake.shape))
         else:
             # UTF-8 arrows look nicer, but cannot be used in LaTeX
             # https://www.w3schools.com/charsets/ref_utf_arrows.asp
             actions = ['^', '<', '_', '>']
-            
+
             print('Lake:')
             print(self.lake)
-        
+
             print('Policy:')
             policy = np.array([actions[a] for a in policy[:-1]])
             print(policy.reshape(self.lake.shape))
-            
+
             print('Value:')
             with _printoptions(precision=3, suppress=True):
                 print(value[:-1].reshape(self.lake.shape))
-                
+
+
 def play(env):
     actions = ['w', 'a', 's', 'd']
-    
+
     state = env.reset()
     env.render()
-    
+
     done = False
     while not done:
         c = input('\nMove: ')
         if c not in actions:
             raise Exception('Invalid action')
-            
+
         state, r, done = env.step(actions.index(c))
-        
+
         env.render()
         print('Reward: {0}.'.format(r))
 
 ################ Model-based algorithms ################
 
- def policy_evaluation(env, policy, gamma, theta, max_iterations):
+def policy_evaluation(env, policy, gamma, theta, max_iterations):
     value = np.zeros(env.n_states, dtype=np.float)
 
     # TODO:
@@ -171,7 +176,7 @@ def policy_iteration(env, gamma, theta, max_iterations, policy=None):
         policy = np.array(policy, dtype=int)
     
     # TODO:
-        
+    vale = 0 # dummy code add by Abdullah
     return policy, value
     
 def value_iteration(env, gamma, theta, max_iterations, value=None):
@@ -181,7 +186,7 @@ def value_iteration(env, gamma, theta, max_iterations, value=None):
         value = np.array(value, dtype=np.float)
     
     # TODO:
-
+    policy = 0 # dummy code add by Abdullah
     return policy, value
 
 ################ Tabular model-free algorithms ################
@@ -362,3 +367,7 @@ def main():
                                    gamma, epsilon, seed=seed)
     policy, value = linear_env.decode_policy(parameters)
     linear_env.render(policy, value)
+
+
+if __name__ == "__main__":
+    main()
