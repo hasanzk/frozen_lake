de91fdb73f401f87b57a635ebf03bc83fadf5101
Hasan Zaki Al-Khabbaz <hasanzk@users.noreply.github.com>
2020-12-08 19:37:11 +0300

    Max steps for testing purposes

--- game.py
+++ game.py
@@ -256,7 +256,7 @@ def main():
               ['.', '#', '.', '#'],
               ['&', '#', '.', '$']]
 
-    env = FrozenLake(lake, slip=0.1, max_steps=len(lake)*len(lake[0]), seed=seed)
+    env = FrozenLake(lake, slip=0.1, max_steps=np.array(lake).size, seed=seed)
     
     print('# Model-based algorithms')
     gamma = 0.9
