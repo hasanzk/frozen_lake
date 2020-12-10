3592786935a4b751e3777a57fa57b1a9198aafff
Hasan Zaki Al-Khabbaz <hasanzk@users.noreply.github.com>
2020-12-10 14:46:29 +0300

    Policy down arrow change

--- FrozenLake.py
+++ FrozenLake.py
@@ -134,14 +134,14 @@ class FrozenLake(Environment):
             print(lake.reshape(self.lake.shape))
         else:
             # UTF-8 arrows look nicer, but cannot be used in LaTeX
-            # https://www.w3schools.com/charsets/ref_utf_arrows.asp
-            actions = ['^', 'ـ', '<', '>']
+            actions = ['^', 'v', '<', '>']
 
             print('Lake:')
             print(self.lake)
 
             print('Policy:')
             policy = np.array([actions[a] for a in policy[:-1]])
+
             policy = policy.reshape(self.lake.shape)
             for i in range(len(self.lake[0])):
                 for j in range(len(self.lake[0])):
